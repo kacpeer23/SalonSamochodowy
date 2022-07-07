6d7af82fd8a16397de557789cba116e277b83bd6
Kacper <kacperkocia@gmail.com>
2022-07-07 22:33:21 +0200

    Dodaj pliki projektów.

---
+++ SalonSamochodowy/Windows1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SalonSamochodowy
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Windows1.xaml
+    /// </summary>
+    public partial class Windows1 : Window
+    {
+        public Windows1()
+        {
+            InitializeComponent();
+            SalonDBEntities db = new SalonDBEntities();
+            var docs = from d in db.Car
+                       select d;
+            foreach (var doc in docs)
+            {
+                Console.WriteLine(doc.Marka);
+                Console.WriteLine(doc.Model);
+            }
+            this.gridCars.ItemsSource = docs.ToList();
+
+        }
+    }
+}
